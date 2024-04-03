4ed361f59758d65976ce0c2acf4fdf2207a30856
Danilo8922 <danlenardial@gmail.com>
2024-04-03 18:53:43 -0300

    eitcha

---
+++ python/modulos/desafios/desafio 16.py
@@ -0,0 +1,4 @@
+import math
+numero = float(input('digite um numero real e eu lhe mostrarei o inteiro '))
+inteiro = math.trunc(numero)
+print(inteiro)
--- python/operadores aritimetricos/desafios/desafio 15.py
+++ python/operadores aritimetricos/desafios/desafio 15.py
@@ -1,11 +1,13 @@
-km_percorrido = float(input('Quantos Km foi percorrido pelo carro? '))
+#variaveis
 dias_alugados = int(input('Por quantos dias ele foi alugado? '))
+km_percorrido = float(input('Quantos Km foi percorrido pelo carro? '))
 aluguel = int(60)
 preço_por_km_percorrido = float(0.15)
 pagar_km_percorrido = km_percorrido * preço_por_km_percorrido
 pagar_dias_alugados = dias_alugados * aluguel
 total_a_pagar = pagar_km_percorrido + pagar_dias_alugados
-print('vc percorreu {}Km e ficou {} dias com o carro')
-print('o aluguel é {} e a tacha dos km percrridos é {}')
-print('o valor do aluguel é {} e atacha dos km percrrido ficou {}' )
-print('o total a pagar é {}')
+#programa funcionando 
+print('Você ficou {} dias com o carro e você percorreu {}Km' .format(km_percorrido, dias_alugados))
+print('A diaria do carro é R${} e a taxa dos km percrridos é R${}' .format(aluguel, preço_por_km_percorrido ))
+print('o valor do aluguel é R${:.2f} e a taxa dos km percrrido ficou R${}'.format(pagar_dias_alugados, pagar_km_percorrido))
+print('o total a pagar do aluguel é R${}' .format(total_a_pagar))
